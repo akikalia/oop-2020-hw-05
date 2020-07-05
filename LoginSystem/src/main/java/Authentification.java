35cc819b35fc3fdc8b933c9f6def5f92879d5b45
akikalia <akika16@freeuni.edu.ge>
2020-07-05 18:37:25 +0400

    final hw5

---
+++ LoginSystem/src/main/java/Authentification.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Authentification extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
---
+++ LoginSystem/src/main/java/Registration.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Registration extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
